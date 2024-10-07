d19c1ce033d3295506107ca285a409a89b3d5b90
akttamanna <akther-t2@ulster.ac.uk>
2024-10-07 12:35:59 +0100

    New method code added to the program

---
+++ Documents/University/Year 2/Object Oriented Programming/Week 2/Methods/Methods/Program.cs
@@ -0,0 +1,97 @@
+﻿//CreateMenu();
+
+void CreateMenu()
+{
+    try
+    {
+        // Construct the menu
+        Console.Clear();
+        Console.WriteLine("Main Menu");
+        Console.WriteLine("1. Say Hello");
+        Console.WriteLine("2. Add Numbers");
+        Console.WriteLine("3. Exit");
+        Console.Write("Choose an option: ");
+
+        // Accept the users choice
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                //Hello method calling 
+                SayHello();
+                break;
+            case "2":
+                AddNumbers();
+                break;
+            case "3":
+                break;
+            default:
+                Console.WriteLine("Invalid choice. Please try again.");
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error. {ex.Message}");
+    }
+}
+
+static void SayHello()
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void AddNumbers()
+{
+    Console.Write("Enter the first number: ");
+    int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter the second number: ");
+    int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    int result = firstNumber + secondNumber;
+    Console.WriteLine($"The result is: {result}");
+}
+
+
+// Method declaration
+// 'CalculateArea' is the method name
+// 'double' is the return type
+// 'double length' and 'double width' are parameters
+
+//Calling main method
+Main();
+
+void Main()
+{
+    try
+    {
+        Console.WriteLine("PLease enter rectangle length");
+        double length = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Please enter rectangle width");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        // Calling the method and storing the result
+        double area = CalculateArea(length, width);
+
+        // Output the result
+        Console.WriteLine($"The area of the rectangle is: {area}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
+
+
+// Method declaration
+// 'CalculateArea' is the method name
+// 'double' is the return type
+// 'double length' and 'double width' are parameters
+static double CalculateArea(double length, double width)
+{
+    // Method body
+    double area = length * width;
+    return area; // Returning the calculated area
+}
